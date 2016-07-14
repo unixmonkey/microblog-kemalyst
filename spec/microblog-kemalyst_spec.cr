f43238f14b0771af26a7b63603cb8ccd973ac4d3
David Jones <david@getfretless.com>
2016-07-13 20:19:36 -0400

    Remove remnants of crystal init.
    As suggested by https://github.com/drujensen/kemalyst

--- spec/microblog-kemalyst_spec.cr
+++
@@ -1,9 +0,0 @@
-require "./spec_helper"
-
-describe Microblog::Kemalyst do
-  # TODO: Write tests
-
-  it "works" do
-    false.should eq(true)
-  end
-end
--- src/microblog-kemalyst/version.cr
+++
@@ -1,3 +0,0 @@
-module Microblog::Kemalyst
-  VERSION = "0.1.0"
-end
